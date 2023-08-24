7b0459fb917517f81f172d36e85865f3f640c459
yangryu321 <yangryu321@gmail.com>
2023-08-24 14:52:00 -0400

    Created entity class Book

---
+++ BookStoreApi/Models/Book.cs
@@ -0,0 +1,18 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace BookStoreApi.Models
+{  
+    public class Books 
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+        [BsonElement("Name")]
+        public string BookName { get; set; } =null!;
+        public decimal Price { get; set; }
+        public string Catagory { get; set; }=null!;
+        public string Author { get; set; }=null!;
+    }
+
+}
